e8896aba0ab723746de0ba18e0ec4cb862838eb8
Aaron <aagavin@gmail.com>
2017-01-22 12:05:15 -0500

    added contact page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let bodyParser = require('body-parser');
 
 let index = require('./routes/index');
 let about = require('./routes/about');
+let contact = require('./routes/contact');
 
 let app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/about', about);
+app.use('/contact', contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
