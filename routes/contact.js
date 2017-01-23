fa666c0c128a3b28c45dca3f61b1be5dfac64495
Aaron <aagavin@gmail.com>
2017-01-23 09:55:14 -0500

    added a *HACK* to get form working

--- routes/contact.js
+++ routes/contact.js
@@ -3,13 +3,14 @@ let router = express.Router();
 
 /* GET contact page. */
 router.get('/', (req, res, next)=>{
-  res.render('contact', {});
+  res.render('contact', {'messageAvailable': false});
 });
 
 router.post('/', (req, res)=>{
 
   // console.log(req);
-  console.log(req.body)
+  console.log(req.body);
+  res.render('contact', {'messageAvailable': true, 'message': `Thanks ${req.body.name} for submitting. As soon as I read your message I'll get back to you at ${req.body.email}`});
   res.end();
 });
 
