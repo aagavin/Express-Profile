bb510c133a6378ed2db7b25b591967b501946634
Aaron <aagavin@gmail.com>
2017-02-05 21:52:46 -0500

    added ablity to add to db

--- routes/contact.js
+++ routes/contact.js
@@ -8,6 +8,8 @@
 */
 
 let express = require('express');
+// let db = require('../db/index').db;
+let EmailList = require('../db/index');
 let router = express.Router();
 
 /* GET contact page. */
@@ -15,11 +17,32 @@ router.get('/', (req, res, next)=>{
   res.render('contact', {'messageAvailable': false});
 });
 
+router.get('/all', (req, res)=>{
+  EmailList.find({}, (err, emailLists)=>{
+    if (err) throw err;
+
+    console.log(emailLists);
+  });
+  res.send('hi');
+});
+
 router.post('/', (req, res)=>{
 
-  // console.log(req);
-  console.log(req.body);
+  let email = new EmailList({
+    name: req.body.name,
+    email: req.body.email,
+    message: req.body.message
+  });
+  email.save((err)=>{
+    if(err){
+      console.err(err);
+    }
+    else{
+      console.log('Saved email');
+    }
+  });
   res.render('contact', {'messageAvailable': true, 'message': `Thanks ${req.body.name} for submitting. As soon as I read your message I'll get back to you at ${req.body.email}`});
+
   res.end();
 });
 
